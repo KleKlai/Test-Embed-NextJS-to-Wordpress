697db5020b1b1ff5eabe9da41d9893e67a83752e
Kleklai <magallenmaynards@gmail.com>
2025-10-19 13:49:11 +0800

    feat: remove bg

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import BookingForm from "@/components/booking-form"
 export default function Home() {
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center">
       <BookingForm/>
     </div>
   )
